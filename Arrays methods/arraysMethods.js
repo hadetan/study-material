18d8829475786ff24e946c8d95c687aeda9d1035
hadetan <aquibsyed83@gmail.com>
2024-05-04 15:36:12 +0530

    concat

--- Arrays methods/arraysMethods.js
+++ Arrays methods/arraysMethods.js
@@ -32,3 +32,12 @@ console.log(num.slice(3, 5));
 let fruit = ["apple", "banana", "orange"];
 fruit.splice(0, 0, "blah");
 console.log(fruit);
+
+//concatenation => to connect multiple arrays into one array.
+let arr1 = [1, 2 ,3 ,4];
+let arr2 = [5, 6, 7, 8];
+let arr3 = [9, 10, 11, 12];
+
+console.log(arr1.concat(arr2));
+console.log(arr1.concat(arr2, arr3));
+
