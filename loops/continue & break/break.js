58b31d76be8ffd8f4ab77aa9df9f3e3927a5b6cf
hadetan <aquibsyed83@gmail.com>
2024-05-05 05:01:36 +0530

    break & continue

---
+++ loops/continue & break/break.js
@@ -0,0 +1,6 @@
+for(let i = 0; i <= 5; i++){
+    if (i == 3) {
+        break; //it means to terminate the whole loop and come out of it.
+    }
+    console.log(i);
+}
---
+++ loops/continue & break/continue.js
@@ -0,0 +1,6 @@
+for(let i = 0; i <= 5; i++){
+    if (i == 3) {
+        continue; //it means to skip this value
+    }
+    console.log(i);
+}
