b0e60ec709b9206d1debdfaf23c38feae0a80d7c
hadetan <aquibsyed83@gmail.com>
2024-05-06 23:00:46 +0530

    random color generator

---
+++ prototype project/color change game/script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("button");
+
+const randomColor = () => {
+    let val = "0123456789ABCDEF";
+    let hash = "#";
+    for(let i = 0; i < 6; i++){
+        hash = hash + val[Math.floor(Math.random() * 16)];
+    }
+    return hash;
+};
+
+console.log(randomColor());
+
+function changeRandomColor(){
+    document.body.style.backgroundColor = randomColor();
+}
+
+btn.addEventListener("click", changeRandomColor);
