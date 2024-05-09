8b4776843d2daf45a5f098259991f40335ecd57c
hadetan <aquibsyed83@gmail.com>
2024-05-09 19:56:59 +0530

    react

---
+++ react DOM/script.js
@@ -0,0 +1,9 @@
+const rootElement = document.querySelector('.root')
+const element = (
+    <header id="main">
+        <h1 id="h1">Hello</h1>
+        <p>Im a paragraph</p>
+    </header>
+)
+
+ReactDOM.render(element, rootElement)
