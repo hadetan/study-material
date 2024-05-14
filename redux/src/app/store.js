0ed8e9d646600a4953a71e066ae8fdbce1bb531c
hadetan <aquibsyed83@gmail.com>
2024-05-14 18:52:14 +0530

    notes

--- redux/src/app/store.js
+++ redux/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter/counterSlice";
+//while exporting make sure in counterslice there should'nt be any curly braces around it else it will not be able to read it and throw many errors.
 
 export const store = configureStore({
     reducer: {
