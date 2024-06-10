1efe8ebc113a97c084c5e4599e10cb1d95115f71
hadetan <aquibsyed83@gmail.com>
2024-06-10 15:30:44 +0530

    promises

---
+++ logics/promises.js
@@ -0,0 +1,17 @@
+function orderPizza () {
+    return new Promise(function(res, rej) {
+        setTimeout(() => {
+            const pizza = "pizza";
+            res(pizza);
+            rej("An error occured while ordering pizza")
+        }, 2000);
+    })
+}
+
+const pizzaPromise = orderPizza();
+
+pizzaPromise.then((pizza) =>{
+    console.log(`my ${pizza}`)
+}).catch(error => {
+    console.log(error)
+})
