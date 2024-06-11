229f23f7ce3c80bc5d60896d22a2e61aaf07b51e
hadetan <aquibsyed83@gmail.com>
2024-06-11 16:34:12 +0530

    local storage

---
+++ local storage/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const essayInput = document.getElementById("essay");
+    essayInput.value = localStorage.getItem("essay")
+})
+
+const essayInput = document.getElementById("essay");
+
+
+essayInput.addEventListener("change", () => {
+    localStorage.setItem("essay", essayInput.value)
+})
